6ecc0d454ef420be1905504a6d37cfb46e7aa287
Akshar-Saxena <aksharsaxena2003@gmail.com>
2023-10-10 19:23:32 +0530

    updated initial pos of car, added comments

--- gameLogic.js
+++ gameLogic.js
@@ -1,13 +1,24 @@
+// Canvas Tools
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
+
+// Audios and Effects
 const sideCrash = new Audio("assets/sideCrash.wav")
+
+// Rendering Car Image
 const carImage = document.querySelector('#car')
 
-var roadSpeed = 2
 
-var xVelocity = 0
-var yVelocity = 0
+// initial roadspeed 0 since car is in state of rest
+var roadSpeed = 0
 
+
+// position of car in X and Y direction
+var xVelocity = 105
+var yVelocity = 95
+
+
+// co-ordinates of road
 const roadMarks = [
     {
         x: 149,
@@ -42,8 +53,8 @@ const roadMarks = [
 ]
 
 
-
 function gameLoop() {
+    // rendering road
     for (let i in roadMarks) {
         ctx.clearRect(roadMarks[i].x, roadMarks[i].y, roadMarks[i].width, roadMarks[i].height)
         if (roadMarks[0].y >= -10 && roadMarks[0].y <= 16) {
@@ -55,33 +66,43 @@ function gameLoop() {
         ctx.fillStyle = "white"
         ctx.fillRect(roadMarks[i].x, roadMarks[i].y, roadMarks[i].width, roadMarks[i].height)
     }
+
+    // checking for side crash
     if (xVelocity == 90 || xVelocity == 185) {
         ctx.clearRect(carImage, xVelocity, yVelocity, 30, 45)
         xVelocity = (xVelocity == 90) ? xVelocity + 5 : xVelocity - 5
         sideCrash.play()
     }
+
+    // updating the position of car
     ctx.imageSmoothingEnabled = false;
     ctx.drawImage(carImage, xVelocity, yVelocity, 30, 45)
     window.requestAnimationFrame(gameLoop)
 }
 
+// Key Controls
 document.onkeydown = (e) => {
     ctx.clearRect(xVelocity, yVelocity, 30, 45)
+    // updating the value of xVelocity i.e position of car in X direction
     if (e.key == "ArrowRight") {
         xVelocity += 5
     }
     else if (e.key == "ArrowLeft") {
         xVelocity -= 5
     }
+    // updating the value of roadSpeed
     else if (e.key == "ArrowUp") {
-        yVelocity -= 5
+        roadSpeed += 1
     }
     else if (e.key == "ArrowDown") {
-        yVelocity += 5
+        if(roadSpeed > 0){
+            roadSpeed -= 1
+        }
     }
     else {
         null
     }
 }
 
+// initiaing GameLoop
 window.requestAnimationFrame(gameLoop)
